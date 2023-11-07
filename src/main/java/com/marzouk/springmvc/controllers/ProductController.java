60e0a75fe0e4debee8798d98a05ff5ee821b0feb
Abderrahim Marzouk <amarzouk2004@yahoo.fr>
2023-11-07 22:41:29 +0100

    first commit

--- src/main/java/com/marzouk/springmvc/controllers/ProductController.java
+++ src/main/java/com/marzouk/springmvc/controllers/ProductController.java
@@ -5,13 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.marzouk.springmvc.dao.ProductDao;
 import com.marzouk.springmvc.model.Product;
